ab7ee560751c6a22abe7092fc655ea0cbaf6797c
JStameus <joseph.stameus@gmail.com>
2021-05-23 21:52:41 +0200

    made a functioning(if a bit janky) implementation of mdparser to render nice looking html when reading documents

--- models/LibraryModel.js
+++ models/LibraryModel.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import {execSync} from "child_process";
 
 const libraries = JSON.parse(fs.readFileSync(path.resolve("./data/testdb.json")));
 
@@ -18,7 +19,7 @@ function loadLibrary(id) {
 // TODO: This will only be used for the active library, so maybe the user's
 // active library should be read from within the function instead of being
 // supplied as an argument?
-function loadDocumentChapter(libraryId, documentId, chapterIndex) {
+function loadDocument(libraryId, documentId, chapterIndex) {
     const library = libraries.find(obj => obj.id === libraryId); 
     if(!library) {
         // TODO: Add proper error handling
@@ -26,25 +27,34 @@ function loadDocumentChapter(libraryId, documentId, chapterIndex) {
         return false;
     }
 
-    const parentDocument = library.documents.find(obj => obj.document_id === documentId);
+    let parentDocument = library.documents.find(obj => obj.document_id === documentId);
     if (!parentDocument) {
         console.log(`Could not find document with ID '${documentId}'`);
         return false;
     }
 
-    if (chapterIndex > parentDocument.chapters.length) {
-        console.log("Chapter out of range");
-        return false;
+    let chapter = parentDocument.chapters[chapterIndex];
+    if (chapterIndex > parentDocument.chapters.length || !parentDocument.chapters[chapterIndex]) {
+        chapter = parentDocument.chapters[0];    
     }
     
     // TODO: This is also the part where the markdown should be parsed and
     // translated into HTML
-    const chapter = parentDocument.chapters[chapterIndex];
+    let markdownPath = path.resolve(chapter.path);
+    let markdownData = fs.readFileSync(markdownPath, {encoding: "utf-8"});
+    // TODO: I need to figure out how to save the output instead of writing an
+    // actual HTML file. Seems a bit excessive and unoptimized. This solution is
+    // super janky and fixing it should be a priority.
+    let chapterFolder = path.dirname(path.resolve(chapter.path));
+    let outputPath = `${chapterFolder}/output.html`;
+    execSync(`./mdparser ${markdownPath} ${outputPath}`);
+    let htmlOutput = fs.readFileSync(outputPath, {encoding: "utf-8"});
+    // TODO: There are also a lot of synchronous functions being used here, on
+    // top of a lot of reading/writing. This may be a performance issue.
     return {
-        markdown: fs.readFileSync(path.resolve(chapter.path), {
-            encoding: "utf-8"
-        })
+        markdown: markdownData,
+        output: htmlOutput
     };
 }
 
-export default {loadLibrary, loadDocumentChapter};
+export default {loadLibrary, loadDocument};
