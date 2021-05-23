e44366971e20c5a3d2fe1bce6d85ac1bb28daed5
JStameus <joseph.stameus@gmail.com>
2021-05-23 16:15:50 +0200

    made it possible to read documents by selecting them in the browse view from the dashboard, and return from reading a document. added temporary in-line styling. fixed tags not rendering correctly. setup route for getting specific documents and added some todo-notes

--- models/LibraryModel.js
+++ models/LibraryModel.js
@@ -5,7 +5,6 @@ const libraries = JSON.parse(fs.readFileSync(path.resolve("./data/testdb.json"))
 
 // TODO: All of this is just for quick prototyping - need to implement actual
 // MongoDB database with mongoose!
-
 function loadLibrary(id) {
     const library = libraries.find(obj => obj.id === id); 
     if(!library) {
@@ -16,4 +15,36 @@ function loadLibrary(id) {
     }
 }
 
-export default {loadLibrary};
+// TODO: This will only be used for the active library, so maybe the user's
+// active library should be read from within the function instead of being
+// supplied as an argument?
+function loadDocumentChapter(libraryId, documentId, chapterIndex) {
+    const library = libraries.find(obj => obj.id === libraryId); 
+    if(!library) {
+        // TODO: Add proper error handling
+        console.log("Could not find library");
+        return false;
+    }
+
+    const parentDocument = library.documents.find(obj => obj.document_id === documentId);
+    if (!parentDocument) {
+        console.log(`Could not find document with ID '${documentId}'`);
+        return false;
+    }
+
+    if (chapterIndex > parentDocument.chapters.length) {
+        console.log("Chapter out of range");
+        return false;
+    }
+    
+    // TODO: This is also the part where the markdown should be parsed and
+    // translated into HTML
+    const chapter = parentDocument.chapters[chapterIndex];
+    return {
+        markdown: fs.readFileSync(path.resolve(chapter.path), {
+            encoding: "utf-8"
+        })
+    };
+}
+
+export default {loadLibrary, loadDocumentChapter};
--- notes.md
+++ notes.md
@@ -3,3 +3,7 @@
 2. Display user-defined sections
 3. Display all documents
 4. Load document chapter by chapter when clicked on
+(Do I create separate models/controllers for documents? They are stored in their
+respective libraries, so they still need to get THAT information somewhere)
+
+
