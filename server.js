0fce0acb891e1cadc84b26873fbccb27aed3e7bb
JStameus <joseph.stameus@gmail.com>
2021-05-29 11:16:29 +0200

    started implementing basic authentication by setting up get and post routes for login/register, and adding simple ejs pages with forms

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 import express from "express";
+import session from "express-session";
 import router from "./routes/router.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+const SESSION_LIFETIME = 1000 * 60 * 60;
 
 dotenv.config();
 const {
     PORT,
     DB_CONNECTION_STRING,
+    SESSION_NAME,
+    SESSION_SECRET,
+    NODE_ENV
 } = process.env;
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(router.routes);
+app.use(session({
+    name: SESSION_NAME,
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        sameSite: true,
+        maxAge: SESSION_LIFETIME,
+        secure: NODE_ENV === "production"
+    }
+}));
+
+// TODO: How to store users/sessions?
 mongoose.connect(DB_CONNECTION_STRING, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 })
-    .then(() => console.log("[ INFO ] Connected to DB."))
-    .catch(() => console.log("[ ERROR ] Failed to connect to DB."));
+.then(() => console.log("[ INFO ] Connected to DB."))
+.catch(() => console.log("[ ERROR ] Failed to connect to DB."));
 
 
 app.listen(PORT, () => {
